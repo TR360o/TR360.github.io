f5adc50490a2467b8d14bd5019e9d750c2ff6324
Coenders,Tijmen T.C.P <70951009+TijmenC@users.noreply.github.com>
2023-12-10 13:44:39 +0100

    Fixed imports again

--- src/script.js
+++ src/script.js
@@ -62,7 +62,7 @@ textGroup.position.x = 0;
 scene.add(textGroup3);
 
 const fontLoader = new FontLoader();
-fontLoader.load("/fonts/Varela_Round_Regular.json", (font) => {
+fontLoader.load("../static/fonts/Varela_Round_Regular.json", (font) => {
   const textGeometryH1 = new TextGeometry("Welcome t    my space", {
     font: font,
     size: 5,
@@ -155,7 +155,7 @@ textGroup.add(triangle);
 //Group 2 About me
 
 const fontLoader2 = new FontLoader();
-fontLoader2.load("/fonts/Varela_Round_Regular.json", (font) => {
+fontLoader2.load("../static/fonts/Varela_Round_Regular.json", (font) => {
   const textGeometryH1 = new TextGeometry("About Me", {
     font: font,
     size: 2.5,
@@ -186,7 +186,7 @@ fontLoader2.load("/fonts/Varela_Round_Regular.json", (font) => {
 // Group3 works
 
 const fontLoader3 = new FontLoader();
-fontLoader2.load("/fonts/Varela_Round_Regular.json", (font) => {
+fontLoader2.load("../static/fonts/Varela_Round_Regular.json", (font) => {
   const textGeometryH3 = new TextGeometry("Works", {
     font: font,
     size: 2.5,
@@ -215,7 +215,7 @@ fontLoader2.load("/fonts/Varela_Round_Regular.json", (font) => {
 
 });
 
-const thikaTexture = new THREE.TextureLoader().load("/assets/thikaCube.png");
+const thikaTexture = new THREE.TextureLoader().load("./assets/thikaCube.png");
 var cgeometry = new THREE.BoxGeometry(15, 15, 0.8);
 var material = new THREE.MeshBasicMaterial({ map: thikaTexture });
 var cube = new THREE.Mesh(cgeometry, material);
