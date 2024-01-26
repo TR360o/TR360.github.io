cd9246a5a8382b45469027ae7f3b2cce169ae794
TR360o <90333491+TR360o@users.noreply.github.com>
2024-01-26 14:47:43 +0100

    Updated gitignore

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,6 @@ export default {
     outDir: "../dist", // Output in the dist/ folder
     emptyOutDir: true, // Empty the folder first
     sourcemap: true, // Add sourcemap
+    target: "ES2022"
   },
 };
